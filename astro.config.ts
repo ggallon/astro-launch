d7d23ae28ce6216828f953948975f30175cbd9da
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-03-15 00:35:15 +0100

    Bump dependencies

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,16 @@
-import db from '@astrojs/db';
-import node from '@astrojs/node';
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
+import db from "@astrojs/db";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
+// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		db(),
-		tailwind({
-			nesting: true,
-		}),
-	],
-	output: 'server',
-	adapter: node({ mode: 'standalone' }),
+  integrations: [
+    db(),
+    tailwind({
+      nesting: true,
+    }),
+  ],
+  output: "server",
+  adapter: vercel(),
 });
